f0b3f5eca0870424700e24f3e2b4411c4f49a807
Andrew <anpilley@microsoft.com>
2017-12-22 23:15:05 -0800

    triangles

---
+++ gasket3.js
@@ -0,0 +1,121 @@
+"use strict";
+
+var canvas;
+var gl;
+
+var points = [];
+var colors = [];
+
+const numTimesToSubdivide = 4;
+
+function triangle(a, b, c, color) {
+
+  var baseColors = [
+    vec3(1.0, 0.0, 0.0),
+    vec3(0.0, 1.0, 0.0),
+    vec3(0.0, 0.0, 1.0),
+    vec3(0.0, 0.0, 0.0)
+  ];  
+
+  colors.push(baseColors[color]);
+  points.push(a);
+  colors.push(baseColors[color]);
+  points.push(b);
+  colors.push(baseColors[color]);
+  points.push(c);
+}
+
+function tetra(a, b, c, d) {
+  triangle(a, c, b, 0);
+  triangle(a, c, d, 1);
+  triangle(a, b, d, 2);
+  triangle(b, c, d, 3);
+}
+
+function divideTetra(a, b, c, d, count) {
+  if (count === 0) {
+    tetra(a, b, c, d);
+  }
+  else {
+    var ab = mix(a, b, 0.5);
+    var ac = mix(a, c, 0.5);
+    var ad = mix(a, d, 0.5);
+    var bc = mix(b, c, 0.5);
+    var bd = mix(b, d, 0.5);
+    var cd = mix(c, d, 0.5);
+
+    --count;
+
+    divideTetra(a, ab, ac, ad, count);
+    divideTetra(ab, b, bc, bd, count);
+    divideTetra(ac, bc, c, cd, count);
+    divideTetra(ad, bd, cd, d, count);
+  }
+}
+
+function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, points.length);
+};
+
+window.onload = function init() {
+  var canvas = document.getElementById("gl-canvas");
+
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
+
+
+  // set up vertex data.
+  var vertices = [
+    vec3(0.0, 0.0, -1.0),
+    vec3(0.0, 0.9428, 0.3333),
+    vec3(-0.8165, -0.4714, 0.3333),
+    vec3(0.8165, -0.4714, 0.3333)
+  ];
+
+  // for(var i = 0; points.length < numPoints; ++i)
+  // {
+  //   var j = Math.floor(Math.random() * 4);
+
+  //   points.push(mix(points[i], vertices[j], 0.5));
+  // }
+
+  divideTetra(vertices[0], vertices[1], vertices[2], vertices[3], numTimesToSubdivide);
+
+
+  // configure WebGL.
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0 );
+  gl.enable(gl.DEPTH_TEST);
+
+  // load shaders
+
+  var program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
+
+  // load data into the GPU
+
+
+  var cBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+  var vColor = gl.getAttribLocation(program, "vColor");
+  gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vColor);
+
+
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+  // associate out shader variables with our data buffer
+
+  var vPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+
+  render();
+};
